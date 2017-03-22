f6ac63ec47e9100bc6fed7406480852fa6221016
sambavsphere@gmail.com <sambavsphere@gmail.com>
2017-03-22 11:51:42 +0530

    2017-03-22

---
+++ module2/a.py
@@ -0,0 +1,14 @@
+
+a=20
+def fun1():
+	return "this is fun1"
+def fun():
+	return 200
+
+
+if __name__ == "__main__":
+	print "program started"
+	res = fun()
+	print "result=",res
+	print "program ended"
+	print "modified content after executing five times"
