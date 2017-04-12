3d6ef7975bf07581614a080244c9bfb076c2548f
sambavsphere@gmail.com <sambavsphere@gmail.com>
2017-04-12 14:46:41 +0530

    2017-04-11

--- a.py
+++ a.py
@@ -1,3 +1,19 @@
-a=10
-b=20
-print a+b
+
+"""
+module to demonstarate pep8 and pylint
+"""
+print "*******************************************************************************\
+*************************"
+A = 10
+B = 20
+C = 30
+
+D = 40
+def add1(product, customer, supplier):
+    """
+    sfsf
+    """
+    return product+customer+supplier
+#print add1(A, B, C)
+print "###################################################\
+####################################"
